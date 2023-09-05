1dd1e4d43609925fa9a5a793ce6149c23e9fb226
akhfarisfz <akhfarifz6017@gmail.com>
2023-09-05 21:02:18 +0700

    Update personalisasi

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ from transformers import AutoTokenizer, AutoModel
 from nlp_id.lemmatizer import Lemmatizer
 from flask import Flask, request, render_template, send_file
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+import pickle
 
 # Inisialisasi Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 # Fungsi untuk melakukan preprocessing pada teks
 def preprocess_text(text):
@@ -133,6 +137,160 @@ def download_template():
 
     # Kembalikan file CSV sebagai tautan unduhan
     return send_file(csv_filename, as_attachment=True)
+
+
+
+
+# Fungsi untuk mengubah data masukan
+def transform_data(form_data):
+    def tr_nilai_matkul_bahasa_inggris_1(x):
+        if x >= 80:
+            return 7
+        elif x >= 75:
+            return 6
+        elif 70 > x >= 65:
+            return 5
+        elif 75 > x >= 70:
+            return 4
+        elif x >= 60:
+            return 3
+        elif x >= 55:
+            return 2
+        elif x >= 45:
+            return 1
+        else:
+            return 0
+
+    def tr_nilai_matematika_sma_kelas_12(x):
+        return float(x)
+
+    def tr_memiliki_beasiswa(x):
+        unique_value = ["Tidak", "Iya"]
+        return unique_value.index(x)
+
+    def tr_waktu_khusus_belajar(x):
+        unique_value = ["Tidak", "Iya"]
+        return unique_value.index(x)
+
+    def tr_suka_lomba(x):
+        unique_value = ["Sangat tidak suka", "Tidak suka", 'Netral', 'Suka', 'Sangat suka']
+        return unique_value.index(x)
+
+    def tr_total_mengikuti_lomba(x):
+        unique_value = ['0', '1-3', '4-10', '11-20', '>20']
+        return unique_value.index(x)
+
+    def tr_pendidikan_ibu(x):
+        unique_value = ['Tidak lulus SD', 'SD/sederajat', 'SMP/sederajat', 'SMA/sederajat', 'D1-D3', 'D4/Sarjana Terapan', 'S1/sederajat', 'S2/sederajat']
+        return unique_value.index(x)
+
+    def tr_pendidikan_ayah(x):
+        unique_value = ['Tidak lulus SD', 'SD/sederajat', 'SMP/sederajat', 'SMA/sederajat', 'D1-D3', 'D4/Sarjana Terapan', 'S1/sederajat', 'S2/sederajat']
+        return unique_value.index(x)
+
+    def tr_penghasilan_orang_tua(x):
+        unique_value = ['< 2.000.000', '2.000.000 - 4.000.000', '4.000.000 - 8.000.000', '8.000.000 - 40.000.000', '> 40.000.000']
+        return unique_value.index(x)
+
+    def tr_estimasi_waktu_perjalanan_ke_kampus(x):
+        unique_value = ['<15', '15-30', '30-60', '60-120', '>120']
+        return unique_value.index(x)
+
+    def tr_tempat_tinggal_kuliah_kos(x):
+        unique_value = ["Tidak", "Iya"]
+        return unique_value.index(x)
+
+    form_data['nilai_matkul_bahasa_inggris_1'] = tr_nilai_matkul_bahasa_inggris_1(form_data['nilai_matkul_bahasa_inggris_1'])
+    form_data['nilai_matematika_sma_kelas_12'] = tr_nilai_matematika_sma_kelas_12(form_data['nilai_matematika_sma_kelas_12'])
+    form_data['memiliki_beasiswa'] = tr_memiliki_beasiswa(form_data['memiliki_beasiswa'])
+    form_data['waktu_khusus_belajar'] = tr_waktu_khusus_belajar(form_data['waktu_khusus_belajar'])
+    form_data['suka_lomba'] = tr_suka_lomba(form_data['suka_lomba'])
+    form_data['total_mengikuti_lomba'] = tr_total_mengikuti_lomba(form_data['total_mengikuti_lomba'])
+    form_data['pendidikan_ibu'] = tr_pendidikan_ibu(form_data['pendidikan_ibu'])
+    form_data['pendidikan_ayah'] = tr_pendidikan_ayah(form_data['pendidikan_ayah'])
+    form_data['penghasilan_orang_tua'] = tr_penghasilan_orang_tua(form_data['penghasilan_orang_tua'])
+    form_data['estimasi_waktu_perjalanan_ke_kampus'] = tr_estimasi_waktu_perjalanan_ke_kampus(form_data['estimasi_waktu_perjalanan_ke_kampus'])
+    form_data['tempat_tinggal_kuliah_kos'] = tr_tempat_tinggal_kuliah_kos(form_data['tempat_tinggal_kuliah_kos'])
+
+    return form_data
+
+# Fungsi untuk memprediksi cluster
+def predict_cluster(data):
+    with open('./tools/scaler.pkl', 'rb') as scaler_path:
+        scaler = pickle.load(scaler_path)
+    with open('./model/model_kprototype.pkl', 'rb') as model_path:
+        model_kprototype = pickle.load(model_path)
+
+    scaled_columns = [
+        'memiliki_beasiswa',
+        'nilai_matkul_bahasa_inggris_1',
+        'nilai_matematika_sma_kelas_12',
+        'suka_lomba',
+        'total_mengikuti_lomba',
+        'pendidikan_ibu',
+        'pendidikan_ayah',
+        'penghasilan_orang_tua',
+        'estimasi_waktu_perjalanan_ke_kampus',
+    ]
+
+    data[scaled_columns] = scaler.transform(data[scaled_columns])
+    data = data.drop(columns=['nama'])
+    if 'cluster' in data.columns:
+        data = data.drop(columns=['cluster'])
+
+    cluster = model_kprototype.predict(data, categorical=[9])
+    del scaler
+    del model_kprototype
+    return cluster
+
+@app.route('/personalisasi')
+def personalisasi():
+    return render_template('personalisasi.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    form_data = {
+        'nama': request.form['nama'],
+        'nilai_matkul_bahasa_inggris_1': float(request.form['nilai_matkul_bahasa_inggris_1']),
+        'nilai_matematika_sma_kelas_12': float(request.form['nilai_matematika_sma_kelas_12']),
+        'memiliki_beasiswa': request.form['memiliki_beasiswa'],
+        'waktu_khusus_belajar': request.form['waktu_khusus_belajar'],
+        'suka_lomba': request.form['suka_lomba'],
+        'total_mengikuti_lomba': request.form['total_mengikuti_lomba'],
+        'pendidikan_ibu': request.form['pendidikan_ibu'],
+        'pendidikan_ayah': request.form['pendidikan_ayah'],
+        'penghasilan_orang_tua': request.form['penghasilan_orang_tua'],
+        'estimasi_waktu_perjalanan_ke_kampus': request.form['estimasi_waktu_perjalanan_ke_kampus'],
+        'tempat_tinggal_kuliah_kos': request.form['tempat_tinggal_kuliah_kos'],
+    }
+
+    # Ubah data formulir menggunakan fungsi transform_data
+    transformed_data = transform_data(form_data)
+
+    # Masukkan data yang telah diubah ke dalam DataFrame
+    df = pd.DataFrame([transformed_data])
+
+    # Prediksi cluster untuk data tersebut
+    df['cluster'] = predict_cluster(df)
+
+    # Visualisasi analisis cluster
+    sns.set(style="whitegrid")  # Gaya plot seaborn
+
+    # Hitung jumlah data dalam setiap cluster
+    cluster_counts = df['cluster'].value_counts().reset_index()
+    cluster_counts.columns = ['Cluster', 'Count']
+
+    # Buat plot bar untuk jumlah data dalam setiap cluster
+    plt.figure(figsize=(8, 6))
+    ax = sns.barplot(x='Cluster', y='Count', data=cluster_counts, palette='viridis')
+    ax.set(xlabel='Cluster', ylabel='Jumlah Data', title='Distribusi Data dalam Setiap Cluster')
+
+    # Simpan plot sebagai gambar
+    plot_filename = 'cluster_distribution.png'
+    plt.savefig(plot_filename, format='png')
+
+    # Tampilkan hasil personalisasi di halaman hasil bersama dengan gambar plot
+    return render_template('hasil.html', data=transformed_data, cluster=df['cluster'].values[0], plot_filename=plot_filename)
 
 # Menjalankan aplikasi Flask
 if __name__ == '__main__':
